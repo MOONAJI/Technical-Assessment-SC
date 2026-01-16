f1ec729004b1497b729aca8a3d0fc5e0ed12f072
Rian Munaji <rianprasetya513@gmail.com>
2026-01-16 23:57:15 +0700

    Add project files

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -7,14 +7,14 @@ import "../src/TarikTambang.sol";
 contract DeployScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        
+
         vm.startBroadcast(deployerPrivateKey);
-        
+
         TarikTambang game = new TarikTambang();
-        
+
         console.log("TarikTambang deployed to:", address(game));
         console.log("Admin address:", game.admin());
-        
+
         vm.stopBroadcast();
     }
 }
